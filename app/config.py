12539895919d1e692ec5b25f762281f12b124e79
risav pyakurel <pyakurel.risav@gmail.com>
2024-03-02 20:52:27 +0545

    third commit

---
+++ app/config.py
@@ -0,0 +1,12 @@
+# from pydantic import BaseSettings, Field
+
+
+# class Settings(BaseSettings):
+#   keyspace : str = Field(..., env= 'ASTRADB_KEYSPACE')
+
+
+#   class Config:
+#     env_file = '.env'
+
+#   def get_settings():
+#       return Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+# from config import Settings
+
 app = FastAPI()
+
 
 @app.get("/")
 def homepage():
-    return {"hello" : "world"} # going to be a json data for REST API server
+    return {"hello" : "world", "keyspace":"Sudarshan"} # going to be a json data for REST API server
     
